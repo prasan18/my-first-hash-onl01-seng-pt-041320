55a7d54769dd57096db37ebbfbd35e2e404ed506
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-25 22:55:56 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,8 +22,7 @@ def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+                  "oil paintings"=>3}
    shipping_manifest["muskets"] = 2
 shipping_manifest["gun powder"] = 4
 puts shipping_manifest
